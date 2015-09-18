9bcaaabf45999e1459f0f83a2f85deadf6e29176
Utkarsh Nadkarni <unadkarni@UtkarshkarniMBP.home>
2015-09-18 17:06:59 -0400

    Initial checkin

---
+++ app/src/test/java/io/pivotal/cc/app/AppApplicationTests.java
@@ -0,0 +1,16 @@
+package io.pivotal.cc.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = AppApplication.class)
+public class AppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ clusterside/src/main/java/io/pivotal/cc/clusterside/ClustersideApplication.java
@@ -0,0 +1,12 @@
+package io.pivotal.cc.clusterside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClustersideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClustersideApplication.class, args);
+    }
+}
